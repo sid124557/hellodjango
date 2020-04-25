5ad13c9266030b1e6e54cb392d7902d97220141a
siddharth <siddharth@localhost.localdomain>
2020-04-25 11:23:32 +0000

    with index.htnl working

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -50,20 +50,19 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'hellodjango.urls'
-
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [os.path.join(BASE_DIR, "templates")],
+            'APP_DIRS': True,
+            'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+            'django.template.context_processors.debug',
+            'django.template.context_processors.request',
+            'django.contrib.auth.context_processors.auth',
+            'django.contrib.messages.context_processors.messages',
             ],
-        },
+         },
     },
 ]
 
