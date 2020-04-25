6e60a3e0bb7864ddbdafd8389069b6fc3d2b3a8d
siddharth <siddharth@localhost.localdomain>
2020-04-25 11:44:48 +0000

    with my statment

--- homepage/views.py
+++ homepage/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 class HomepageView(TemplateView):
         template_name = 'index.html'
+        def get_context_data(self, **kwargs):
+                context = super().get_context_data(**kwargs)
+                context['my_statement'] = 'Nice to see you!'
+                return context
